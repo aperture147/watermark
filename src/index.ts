b705e50cf3a33b360543fb9825e75ce4be8ef8ca
HaiVQ <me@haivq.com>
2025-04-05 07:49:32 +0000

    refactor: improve handling of robots.txt and favicon responses

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { PhotonImage, watermark, resize } from "@cf-wasm/photon";
 import { env } from "cloudflare:workers";
+import { FAVICON_SET, ROBOTS_TXT } from "./constant"
 
 const TRIMMING_SLASH_REGEX = /^\/+|\/+$/g;
 
@@ -7,7 +8,6 @@ let WATERMARK_IMAGE: PhotonImage | null = null
 let WATERMARK_WIDTH = 0
 let WATERMARK_HEIGHT = 0
 let WATERMARK_RATIO = 0
-
 
 const getWatermarkImage = async () => {
 	if (!WATERMARK_IMAGE) {
@@ -22,26 +22,42 @@ const getWatermarkImage = async () => {
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		// await fetch("https://images.pexels.com/photos/2559941/pexels-photo-2559941.jpeg?w=1280&h=853").then(resp => env.R2_IMAGES.put("test2.jpg", resp.body))
 		const url = new URL(request.url);
 		
-		const objectKey = url.pathname.replace(TRIMMING_SLASH_REGEX, '').replace(/\/{2,}/, '/')
+		const objectKey = url.pathname.replace(TRIMMING_SLASH_REGEX, '')
 		if (!objectKey) {
 			return new Response('No object key provided', { status: 400 });
 		}
 
-		const cache = caches.default
-		const cacheKey = `http://localhost/${objectKey}`
-		let cachedResp = await cache.match(cacheKey, { ignoreMethod: true })
+		if (objectKey === 'robots.txt') {
+			return new Response(ROBOTS_TXT, {
+				headers: {
+					'Content-Type': 'text/plain',
+				}
+			});
+		}
 
+		const cacheKey = `http://localhost/${objectKey}`
+		const cache = caches.default
+
+		let cachedResp = await cache.match(cacheKey, { ignoreMethod: true })
 		if (cachedResp && cachedResp.ok) return cachedResp;
 
 		const imageObject = await env.R2_IMAGES.get(objectKey)
 		if (!imageObject) {
-			return new Response('Image not found', { status: 404 });
+			return new Response('Not found', { status: 404 });
 		}
-		
-		
+
+		if (FAVICON_SET.has(objectKey)) {
+			const resp = new Response(imageObject.body, {
+				headers: {		
+					'Cache-Control': 'public, max-age=31536000, s-maxage=31536000'
+				}
+			})
+			ctx.waitUntil(cache.put(cacheKey, resp.clone()))
+			return resp
+		}
+
 		const image = PhotonImage.new_from_byteslice(new Uint8Array(await imageObject.arrayBuffer()))
 		const imageWidth = image.get_width()
 		const imageHeight = image.get_height()
